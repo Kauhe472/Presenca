72a00102dd7b41472caff06a7ebf93751020838b
Kauhe472 <kauhecezar472@gmail.com>
2023-08-04 17:15:30 -0300

    Atualizado 3.0

--- src/main/java/com/MundoSenai/Presenca/Controller/C_Pessoa.java
+++ src/main/java/com/MundoSenai/Presenca/Controller/C_Pessoa.java
@@ -1,5 +1,6 @@
 package com.MundoSenai.Presenca.Controller;
 
+import ch.qos.logback.core.model.Model;
 import com.MundoSenai.Presenca.Service.S_Pessoa;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,7 +36,17 @@ public class C_Pessoa {
                                @RequestParam("telefone") String telefone,
                                @RequestParam("data_nasc") String data_nascimento,
                                @RequestParam("senha") String senha,
-                               @RequestParam("confsenha") String conf_senha){
+                               @RequestParam("confsenha") String conf_senha,
+                               Model model){
+
+        
+        String mensagem = S_Pessoa.cadastrarPessoas(nome, email, cpf, telefone, data_nasc, senha, confsenha);
+        model.addAttribute("nome") String nome,
+        ("email") String email,
+        ("cpf") String cpf,
+        ("telefone") String telefone,
+        ("data_nasc") String data_nascimento
+
         return "Redirect: /";
     }
 
--- src/main/java/com/MundoSenai/Presenca/Service/S_Pessoa.java
+++ src/main/java/com/MundoSenai/Presenca/Service/S_Pessoa.java
@@ -4,6 +4,8 @@ import com.MundoSenai.Presenca.Model.M_Pessoa;
 import com.MundoSenai.Presenca.Repository.R_Pessoa;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 public class S_Pessoa {
     private static R_Pessoa r_pessoa;
@@ -15,4 +17,33 @@ public class S_Pessoa {
     public static M_Pessoa getPessoaLogin(String usuario, String senha) {
         return r_pessoa.findByUsuarioESenha(Long.valueOf(usuario), senha);
     }
+
+    public static String cadastrarPessoas(String nome, String email,
+                                          String cpf, String telefone, String data_nasc,
+                                          String senha, String confisenha) {
+        String mensagemRetorno = "";
+        if(!senha.equals(confisenha)) {
+            mensagemRetorno += "A senha e a confirmação de senha devem ser iguais";
+        } else if (!CpfValidator.validarCPF(cpf)) {
+            mensagemRetorno += "CPF invalido";
+            else if (nome == null || nome.trim() == "") {
+                mensagemRetorno += "Deve ser informado o Nome\n";
+        } else if ((nome == null || nome.trim() == "") &&
+                (NumberCleaner.cleanNumber(telefone) == null
+                        || NumberCleaner.cleanNumber(telefone).trim() == "")) {
+            return "email ou telefone precisam ser informados"
+        }
+        else{
+            M_Pessoa m_pessoa = new M_Pessoa();
+            m_pessoa.setNome(nome);
+            m_pessoa.setCpf(Long.valueOf(NumberCleaner.cleanNumber(cpf)));
+            m_pessoa.setTelefone(Long.valueOf(NumberCleaner.cleanNumber(telefone)));
+            m_pessoa.setEmail(email);
+            m_pessoa.setData_nasc(LocalDate.parse(data_nasc));
+            m_pessoa.setSenha(senha);
+            r_pessoa.save(m_pessoa);
+        }
+
+        return"Cadastro efetuado com sucesso";
+}
 }
